6cfc0e8404dddc543a44ca45552d7eacabb4149a
TwentySZN <lekan8771@gmail.com>
2025-06-07 10:29:40 +0100

    initial commit

---
+++ String.py
@@ -0,0 +1,37 @@
+# get the length of a string
+
+ceo_name = "Ted lanzo"
+print(len(ceo_name))
+
+# access a character in a string
+print(ceo_name[2])
+# get last character
+print(ceo_name[-1])
+
+#  get first 3 string
+print(ceo_name[0:3])
+
+# escaping character
+language = "python \"programing langauge"
+language2 = "python \nprograming "
+print(language)
+print(language2)
+
+# formattting of string
+first_name = "ted"
+last_name = "lanzo"
+
+print(f"{first_name} {last_name}")
+
+# get index of lan
+print(ceo_name.find("anz"))
+
+# check if a value contain a character
+print("anz" in ceo_name)
+print("anzo" not  in ceo_name)
+
+# get the type a value currently use
+x = 20
+print(isinstance(20,int))
+
+
